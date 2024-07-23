6090df40d69556474ae0cd995b65f14e765cf358
Shiv Rane <59121200+shiv-rane@users.noreply.github.com>
2024-07-23 16:21:45 +0530

    sumofno.java

--- sumofno.java
+++ sumofno.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 public class sumofno {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner scan = new Scanner(System.in);
 		int sum = 0;
 		int n = 50;
