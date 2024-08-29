f7461878ca1b719331da542658a0d0de90ab41a3
Shiv Rane <59121200+shiv-rane@users.noreply.github.com>
2024-08-29 16:42:16 +0530

    Update CountDigits.java

--- CountDigits.java
+++ CountDigits.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
 public class CountDigits {
-
 	public static void main(String[] args) {
 		
-				Scanner scan = new Scanner(System.in);
-				int digits;
-				int count=0;
-				System.out.println("Enter the digit: ");
-				digits = scan.nextInt();
-					if(digits==0) {
-						System.out.println("1");
-					}
-					else {
-						while(digits!=0) {
-							count++;
-							digits=digits/10;
-						}
-						System.out.println(count);
-					}
-
+	Scanner scan = new Scanner(System.in);
+	int digits;
+	int count=0;
+	System.out.println("Enter the digit: ");
+	digits = scan.nextInt();
+		if(digits==0) {
+		System.out.println("1");
+		}
+		else {
+			while(digits!=0) {
+			count++;
+			digits=digits/10;
+			}
+		System.out.println(count);
+		}
 		}
 			
 	}
